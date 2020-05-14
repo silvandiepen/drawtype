4bb9fd81fc676212c5b7b0c2f79658b40b0872f8
Sil van Diepen <me@silvandiepen.com>
2020-05-14 12:27:50 +0200

    seen as new files

---
+++ components/Layout/Header.vue
@@ -0,0 +1,53 @@
+<template>
+	<header id="header" class="header">
+		<h3 class="header__logo">
+			<NuxtLink to="/"> <Logo /> </NuxtLink>
+		</h3>
+		<NavigationMenu />
+	</header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import NavigationMenu from '@/components/navigation/menu.vue';
+import Logo from '@/components/Logo.vue';
+export default Vue.extend({
+	components: {
+		NavigationMenu,
+		Logo
+	},
+	data: () => ({
+		projectName: 'DrawType'
+	})
+});
+</script>
+
+<style lang="scss">
+@import '~tools';
+
+.header {
+	position: relative;
+	top: 0;
+	left: 0;
+	z-index: 1;
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+	background-color: black;
+	color: white;
+	text-align: center;
+	padding: 1em;
+	&__logo {
+		z-index: 2;
+		font-size: 1.5rem;
+		line-height: 1.5rem;
+		padding: 1rem 0;
+		svg {
+			height: 2em;
+		}
+		a {
+			text-decoration: none;
+		}
+	}
+}
+</style>
